557434973366f3881539f88c47612923e68f7771
Michael Beasley <youvegotmoxie@gmail.com>
2026-06-14 11:08:40 -0800

    it's working checkpoint
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
 use std::env;
 use std::fs::File;
 use std::path::PathBuf;
+use std::process::exit;
 extern crate yaml_serde;
 use yaml_serde::Value;
 
 // TODO: return an actual result and use ? instead of .unwrap()
 fn main() {
-    // ~/.kube/config as a yaml Value type
-    let kube_yaml = kubeconfig_to_yaml().unwrap();
-    let current_ctx = get_current_kube_context(&kube_yaml).unwrap();
-    let all_contexts = list_all_contexts(&kube_yaml).unwrap();
+    let input_context: Vec<String> = env::args().collect();
 
-    println!("current context: {}", current_ctx);
+    let kubeconfig_path = build_kubeconfig_path().unwrap();
+    let kube_yaml = kubeconfig_to_yaml(kubeconfig_path.clone()).unwrap();
 
-    for ctx in all_contexts.iter() {
-        println!("{}", ctx);
-    }
+    let new_context = Value::String(String::from(&input_context[1]));
+    set_context(&kubeconfig_path, &kube_yaml, new_context).unwrap();
 }
 
-/// Reads the kubeconfig file and returns its contents as a yaml Value type
-fn kubeconfig_to_yaml() -> Result<Value, Box<dyn std::error::Error>> {
+/// Builds the path to the kubeconfig file, either from the KUBECONFIG env var or the default path
+fn build_kubeconfig_path() -> Result<PathBuf, Box<dyn std::error::Error>> {
     let mut kubeconfig = PathBuf::new();
     // Create the path to ~/.kube/config for reading the file
     // Read the KUBECONFIG env var if set and use that
@@ -27,7 +25,7 @@ fn kubeconfig_to_yaml() -> Result<Value, Box<dyn std::error::Error>> {
         Ok(kube_config_path) => kubeconfig.push(&kube_config_path),
         // If no KUBECONFIG env var is set then use the HOME env var and tack on the rest of the default path
         Err(env::VarError::NotPresent) => {
-            kubeconfig.push(env::var("HOME").unwrap_or_default());
+            kubeconfig.push(env::var("HOME")?);
             kubeconfig.push(".kube");
             kubeconfig.push("config");
         }
@@ -35,7 +33,11 @@ fn kubeconfig_to_yaml() -> Result<Value, Box<dyn std::error::Error>> {
             panic!("Unable to read kubectl config file");
         }
     }
+    Ok(kubeconfig)
+}
 
+/// Reads the kubeconfig file and returns its contents as a yaml Value type
+fn kubeconfig_to_yaml(kubeconfig: PathBuf) -> Result<Value, Box<dyn std::error::Error>> {
     let yaml_data: Value =
         yaml_serde::from_str(&std::io::read_to_string(File::open(&kubeconfig)?)?)?;
 
@@ -45,24 +47,24 @@ fn kubeconfig_to_yaml() -> Result<Value, Box<dyn std::error::Error>> {
 /// Takes kubeconfig as YAML Value from kubeconfig_to_yaml and returns the current context
 fn get_current_kube_context(
     kube_context_yaml: &Value,
-) -> Result<String, Box<dyn std::error::Error>> {
+) -> Result<Value, Box<dyn std::error::Error>> {
     let current_context = kube_context_yaml["current-context"]
         .as_str()
-        .ok_or("current-context key not found")?
-        .to_string();
+        .ok_or("current-context key not found")?;
 
-    Ok(current_context)
+    Ok(current_context.into())
 }
 
 /// Takes kubeconfig as YAML Value from kubeconfig_to_yaml and returns the current context
+#[allow(dead_code)]
 fn list_all_contexts(kube_context_yaml: &Value) -> Result<Vec<String>, Box<dyn std::error::Error>> {
     let mut all_contexts = Vec::new();
     let contexts = kube_context_yaml["contexts"].as_sequence();
 
     if let Some(contexts) = contexts {
-        for context in contexts {
+        for ctx in contexts {
             all_contexts.push(
-                context["name"]
+                ctx["name"]
                     .as_str()
                     .ok_or("name key not found")?
                     .to_string(),
@@ -70,6 +72,37 @@ fn list_all_contexts(kube_context_yaml: &Value) -> Result<Vec<String>, Box<dyn s
         }
     }
     Ok(all_contexts)
+}
+
+fn set_context(
+    kubeconfig: &PathBuf,
+    kube_context_yaml: &Value,
+    new_context: Value,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let mut updated_yaml = kube_context_yaml.clone();
+    let current_context = get_current_kube_context(kube_context_yaml)?;
+
+    if current_context != new_context {
+        updated_yaml["current-context"] = new_context.into();
+        println!(
+            "context set to {}",
+            updated_yaml["current-context"].as_str().unwrap()
+        );
+    } else {
+        println!(
+            "context is already set to {}",
+            new_context.as_str().unwrap()
+        );
+        exit(0)
+    }
+
+    let yaml_data = yaml_serde::to_string(&updated_yaml)?;
+
+    {
+        std::fs::write(&kubeconfig, &yaml_data)?;
+    }
+
+    Ok(yaml_data)
 }
 // what are we doing?
 // function 1
