1d4b06136b9aa75628d7f2485ae557a468d68666
Michael Beasley <youvegotmoxie@gmail.com>
2026-06-14 15:46:40 -0800

    handle more error cases
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,36 @@ use std::process::exit;
 extern crate yaml_serde;
 use yaml_serde::Value;
 
+// TODO:
+// Add error handling for supplying invalid an cluster value, eg supplied input context isn't in the kubeconfig's map of clusters
+// The case where no input is supplied the fallback should be to list all contexts
+// Backup the kubeconfig before modifying the file
+// Make a copy of the original kubeconfig -> edit the copy -> move the copy to the original path -> delete
+// Move non-main functions to a helper crate
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let new_context_cli_input: Vec<String> = env::args().collect();
+    let input_args: Vec<String> = env::args().collect();
 
-    let kubeconfig_path = build_kubeconfig_path()?;
-    let kube_yaml = kubeconfig_to_yaml(kubeconfig_path.clone())?;
+    let path = build_kubeconfig_path()?;
+    let path_buf = &path;
+    let kube_yaml = kubeconfig_to_yaml(path_buf.to_path_buf())?;
 
-    let new_context = Value::String(String::from(&new_context_cli_input[1]));
-    set_context(&kubeconfig_path, &kube_yaml, new_context)?;
+    if let Some(new_context) = input_args.get(1) {
+        set_context(
+            &path_buf,
+            &kube_yaml,
+            Value::String(String::from(new_context)),
+        )?;
+    } else {
+        let all_cluster_names = list_all_contexts(&kube_yaml)?;
+        if all_cluster_names.is_empty() {
+            eprintln!("No clusters found. The config file is either empty or malformed");
+            exit(1);
+        }
+        for name in all_cluster_names {
+            println!("{}", name);
+        }
+    }
     Ok(())
 }
 
@@ -66,7 +88,7 @@ fn list_all_contexts(kube_context_yaml: &Value) -> Result<Vec<String>, Box<dyn s
             all_contexts.push(
                 ctx["name"]
                     .as_str()
-                    .ok_or("name key not found")?
+                    .ok_or("cluster name key not found")?
                     .to_string(),
             );
         }
@@ -74,19 +96,22 @@ fn list_all_contexts(kube_context_yaml: &Value) -> Result<Vec<String>, Box<dyn s
     Ok(all_contexts)
 }
 
+// if no context is supplied then print output of get_current_kube_context
 fn set_context(
     kubeconfig: &PathBuf,
     kube_context_yaml: &Value,
     new_context: Value,
 ) -> Result<String, Box<dyn std::error::Error>> {
     let mut updated_yaml = kube_context_yaml.clone();
-    let current_context = get_current_kube_context(kube_context_yaml)?;
+    let current_context = get_current_kube_context(&kube_context_yaml)?;
 
     if current_context != new_context {
         updated_yaml["current-context"] = new_context.into();
         println!(
             "Updated the current context to use {}",
-            updated_yaml["current-context"].as_str().unwrap()
+            updated_yaml["current-context"]
+                .as_str()
+                .ok_or("current-context key not found")?
         );
         let yaml_data = yaml_serde::to_string(&updated_yaml)?;
         std::fs::write(&kubeconfig, &yaml_data)?;
@@ -94,7 +119,9 @@ fn set_context(
     } else {
         println!(
             "The cluster context is already set to {}",
-            new_context.as_str().unwrap()
+            new_context
+                .as_str()
+                .ok_or("Unable to get current-context key")?
         );
         exit(0)
     }
