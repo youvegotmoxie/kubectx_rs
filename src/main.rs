bcd9cb0c6a2830d3c75779ba4cba6af555cf222b
Michael Beasley <youvegotmoxie@gmail.com>
2026-06-14 09:13:59 -0800

    parity with old version
    Signed-off-by: Michael Beasley <youvegotmoxie@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,73 @@ use std::path::PathBuf;
 extern crate yaml_serde;
 use yaml_serde::Value;
 
+// TODO: return an actual result and use ? instead of .unwrap()
 fn main() {
-    let current_context =
-        ParseKubeConfig::get_current_context(KubeConfig.read_kubeconfig().unwrap()).unwrap();
-    println!(
-        "current cluster context: {}",
-        current_context.cluster.as_str().unwrap()
-    );
+    // ~/.kube/config as a yaml Value type
+    let kube_yaml = kubeconfig_to_yaml().unwrap();
+    let current_ctx = get_current_kube_context(&kube_yaml).unwrap();
+    let all_contexts = list_all_contexts(&kube_yaml).unwrap();
 
-    let all = ParseKubeConfig::get_all_clusters(KubeConfig.read_kubeconfig().unwrap()).unwrap();
-    println!("all clusters:\n{}", all.join("\n"));
+    println!("current context: {}", current_ctx);
+
+    for ctx in all_contexts.iter() {
+        println!("{}", ctx);
+    }
 }
 
+/// Reads the kubeconfig file and returns its contents as a yaml Value type
+fn kubeconfig_to_yaml() -> Result<Value, Box<dyn std::error::Error>> {
+    let mut kubeconfig = PathBuf::new();
+    // Create the path to ~/.kube/config for reading the file
+    // Read the KUBECONFIG env var if set and use that
+    match env::var("KUBECONFIG") {
+        Ok(kube_config_path) => kubeconfig.push(&kube_config_path),
+        // If no KUBECONFIG env var is set then use the HOME env var and tack on the rest of the default path
+        Err(env::VarError::NotPresent) => {
+            kubeconfig.push(env::var("HOME").unwrap_or_default());
+            kubeconfig.push(".kube");
+            kubeconfig.push("config");
+        }
+        Err(_) => {
+            panic!("Unable to read kubectl config file");
+        }
+    }
+
+    let yaml_data: Value =
+        yaml_serde::from_str(&std::io::read_to_string(File::open(&kubeconfig)?)?)?;
+
+    Ok(yaml_data)
+}
+
+/// Takes kubeconfig as YAML Value from kubeconfig_to_yaml and returns the current context
+fn get_current_kube_context(
+    kube_context_yaml: &Value,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let current_context = kube_context_yaml["current-context"]
+        .as_str()
+        .ok_or("current-context key not found")?
+        .to_string();
+
+    Ok(current_context)
+}
+
+/// Takes kubeconfig as YAML Value from kubeconfig_to_yaml and returns the current context
+fn list_all_contexts(kube_context_yaml: &Value) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let mut all_contexts = Vec::new();
+    let contexts = kube_context_yaml["contexts"].as_sequence();
+
+    if let Some(contexts) = contexts {
+        for context in contexts {
+            all_contexts.push(
+                context["name"]
+                    .as_str()
+                    .ok_or("name key not found")?
+                    .to_string(),
+            );
+        }
+    }
+    Ok(all_contexts)
+}
 // what are we doing?
 // function 1
 // read KUBECONFIG
@@ -32,101 +87,3 @@ fn main() {
 // set context to one chosen by user
 // this should take a user supplied cluster context; use what's selected from either tab completion, fzf input or stdin at runtime
 //
-trait KubeConfigTrait {
-    fn read_kubeconfig(&self) -> Result<String, Box<dyn std::error::Error>>;
-}
-
-struct KubeConfig;
-
-#[allow(dead_code)]
-trait KubeContextTrait {
-    fn get_current_context(
-        kube_config: String,
-    ) -> Result<ParseKubeConfig, Box<dyn std::error::Error>>;
-    fn get_all_clusters(kube_config: String) -> Result<Vec<String>, Box<dyn std::error::Error>>;
-    fn set_context(
-        kube_config: Value,
-        new_context: Value,
-        current_context: Value,
-    ) -> Result<Value, Box<dyn std::error::Error>>;
-}
-
-struct ParseKubeConfig {
-    cluster: Value,
-}
-
-impl KubeConfigTrait for KubeConfig {
-    /// Read the kubectl config from either the KUBECONFIG env var
-    ///
-    /// Fall back to reading from the well known ~/.kube/config path if that value isn't set
-    fn read_kubeconfig(&self) -> Result<String, Box<dyn std::error::Error>> {
-        let mut kubeconfig = PathBuf::new();
-
-        // Create the path to ~/.kube/config for reading the file
-        // Read the KUBECONFIG env var if set and use that
-        match env::var("KUBECONFIG") {
-            Ok(kube_config_path) => kubeconfig.push(&kube_config_path),
-            // If no KUBECONFIG env var is set then use the HOME env var and tack on the rest of the default path
-            Err(env::VarError::NotPresent) => {
-                kubeconfig.push(env::var("HOME")?);
-                kubeconfig.push(".kube");
-                kubeconfig.push("config");
-            }
-            Err(_) => {
-                panic!("Unable to read kubectl config file");
-            }
-        }
-
-        // Read the kubeconfig and return the contents of the file
-        let config_path: PathBuf = kubeconfig;
-        let data = std::io::read_to_string(File::open(&config_path)?)?;
-
-        Ok(data)
-    }
-}
-
-impl KubeContextTrait for ParseKubeConfig {
-    /// Read kubeconfig and get the YAML key for current-context and return that value
-    fn get_current_context(
-        kube_config: String,
-    ) -> Result<ParseKubeConfig, Box<dyn std::error::Error>> {
-        let config = yaml_serde::from_str::<Value>(&kube_config)?;
-        let localhost_default_context =
-            yaml_serde::from_str::<Value>("current-context: localhost")?;
-
-        let cluster: Value = config
-            .get("current-context")
-            .unwrap_or(&localhost_default_context)
-            .clone();
-
-        Ok(ParseKubeConfig { cluster })
-    }
-
-    /// Read kubeconfig and get the YAML key for contexts and return a vector of all cluster names
-    fn get_all_clusters(kube_config: String) -> Result<Vec<String>, Box<dyn std::error::Error>> {
-        let config = yaml_serde::from_str::<Value>(&kube_config)?;
-        let mut cluster_list = Vec::new();
-
-        let all_cluster_contexts = config["contexts"].as_sequence().unwrap();
-
-        for entry in all_cluster_contexts.iter() {
-            let cluster_name = entry["name"]
-                .as_str()
-                .ok_or("Missing entry in context")?
-                .to_string();
-            cluster_list.push(cluster_name);
-        }
-
-        Ok(cluster_list)
-    }
-
-    fn set_context(
-        _kube_config: Value,
-        _new_context: Value,
-        _current_context: Value,
-        // This probably needs to be changed to some fs io result since
-        // it will eventually write changes to the filesystem
-    ) -> Result<Value, Box<dyn std::error::Error>> {
-        unimplemented!()
-    }
-}
